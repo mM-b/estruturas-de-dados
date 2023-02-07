731be3ff74d7e58a344c7b7c7eea040580435937
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-07 17:59:25 -0300

    Add files via upload

---
+++ x y ponteiro função.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int sub(int x,int y){
+    return x - y;
+}
+int soma(int nx, int ny){
+    return nx + ny;
+}
+int calcula(int x, int y, int(*op)(int,int)){
+    return(*op)(x,y);
+}
+int main(void){
+    char x[50];
+int resultado;
+printf("para somar os valores digite 'soma' ");
+scanf("%s",&x);
+if(strcmp(x,"soma")==0){
+    resultado = calcula(5,3,soma);
+    printf("%d", resultado);
+}
+else if(strcmp(x,"sub")==0){
+    resultado = calcula(5,3,sub);
+    printf("%d",resultado);
+}
+}
