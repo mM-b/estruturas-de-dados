5235fcd58b56ac10394761868e0729ae4e4021b8
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-24 11:25:36 -0300

    Update pessoa.c

--- exercicios/pratica union e enum/pessoa.c
+++ exercicios/pratica union e enum/pessoa.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-typedef enum genero
+typedef enum genero // cria um enum para generos
 {
-    MASCULINO,
-    FEMININO,
-    NEUTRO
-
+    MASCULINO,//recebe o valor 0
+    FEMININO//recebe o valor 1
 } Genero;
 
-typedef struct pessoa
+typedef struct pessoa //cria uma estrutura de pessoa
 {
     char nome[50];
     int idade;
@@ -20,11 +18,12 @@ int main(void)
     printf("informe o nome:");
     scanf(" %[^\n]s", p1.nome);
     printf("informe a idade:");
-    scanf("%d",&p1.idade);
+    scanf("%d", &p1.idade);
     printf("informe o genero");
-    scanf("%d",(int*)&p1.genero);
+    scanf("%d", (int *)&p1.genero);
 
-    printf("nome:%s\nidade:%d\n",p1.nome,p1.idade);
-    p1.genero==MASCULINO?printf("Masculino\n"):printf("Feminino");
+    printf("nome:%s\nidade:%d\n", p1.nome, p1.idade);
+    p1.genero == MASCULINO ? printf("Masculino\n") : printf("Feminino");
+    //é uma declaração condicional. Se a variavel genero da estrutura aluno for igual a MASCULINO , Masculino é impresso, senao, Feminino é impresso
     return 0;
 }
