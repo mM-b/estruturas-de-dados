6cc408ca123a3ad8a18f59e6e377eef0ab6fee13
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-24 15:25:16 -0300

    Update q1.c

--- exercicios/ponteiros/q1.c
+++ exercicios/ponteiros/q1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-    int x, y, *p;
-    y = 0;
-    p = &y;
-    x = *p;
-    x = 4;
-    (*p)++;
-    --x;
-    (*p) += x;
+    int x, y, *p;                       // declaração de variaveis
+    y = 0;                              // y recebe 0
+    p = &y;                             // p recebe o endereço de y
+    x = *p;                             // x recebe o endereço que p aponta
+    x = 4;                              // x recebe 4
+    (*p)++;                             // o endereço que p aponta é incrementado
+    --x;                                // decrementa o valor de x
+    (*p) += x;                          // o conteudo onde p aponta é somado com x
 
-    printf("x= %d y=%d p=%p", x, y, p);
+    printf("x= %d y=%d p=%p", x, y, p); 
     return 0;
 }
