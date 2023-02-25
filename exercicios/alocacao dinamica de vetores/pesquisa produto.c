f8592945d12f1986b0a698b2c0e0fb068ebe9ede
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-25 00:14:44 -0300

    Update pesquisa produto.c

--- exercicios/alocacao dinamica de vetores/pesquisa produto.c
+++ exercicios/alocacao dinamica de vetores/pesquisa produto.c
@@ -8,21 +8,20 @@ int main(void)
     float FG = 0, MN = 0;
     printf("informe a quantidade pessoas");
     scanf("%f", &npess);
-    
 
-    char *sexo = (char *)malloc(30 * sizeof(char));
-    char **opiniao = (char **)malloc(npess * sizeof(char *));
-    if (sexo == NULL)
+    char *sexo = (char *)malloc(30 * sizeof(char));           // aloca dinamicamente  um vetor de char
+    char **opiniao = (char **)malloc(npess * sizeof(char *)); // aloca dinamicamente uma matriz de char
+    if (sexo == NULL)                                         // verifica se a alocação foi bem sucedida
     {
-        exit(1);
+        exit(1); // aborta a execução
     }
     if (opiniao == NULL)
     {
         exit(1);
     }
-    for (int j = 0; j < npess; j++)
+    for (int j = 0; j < npess; j++) // percorre as linhas da matriz
     {
-        opiniao[j] = (char *)malloc(50 * sizeof(char));
+        opiniao[j] = (char *)malloc(50 * sizeof(char)); // aloca dinamicamente um vetor de char em opniao[j]
     }
     for (int j = 0; j < npess; j++)
     {
@@ -30,24 +29,24 @@ int main(void)
         scanf(" %[^\n]s", sexo);
         printf("qual sua opiniao sobre o produto?(respostas validas:gostei,nao gostei)");
         scanf(" %[^\n]s", opiniao[j]);
-        if ((strcmp(sexo,"F") == 0) && (strcmp(opiniao[j], "gostei") == 0))
+        if ((strcmp(sexo, "F") == 0) && (strcmp(opiniao[j], "gostei") == 0)) // se as string forem iguais a função retorna zero, verificando que as condições foram atendidas
         {
-            FG++;
+            FG++; // conta a quantidade de pessoas do sexo feminino que gostaram
         }
         if ((strcmp(sexo, "M") == 0) && (strcmp(opiniao[j], "nao gostei") == 0))
         {
-            MN++;
+            MN++; // conta a quantidade de pessoas do sexo masculino que não gostaram
         }
     }
 
-    printf("porcentagem de pessoas do sexo feminino que responderam que gostaram do produto: %f\nporcentagem de pessoas do sexo masculino que responderam que nao gostaram do produto:%f", (FG/npess)*100.0, (MN/npess)*100.0);  
+    printf("porcentagem de pessoas do sexo feminino que responderam que gostaram do produto: %f\nporcentagem de pessoas do sexo masculino que responderam que nao gostaram do produto:%f", (FG / npess) * 100.0, (MN / npess) * 100.0);
 
     free(sexo);
-    for (int j = 0; j < npess; j++)
+    for (int j = 0; j < npess; j++) // percorre todas as posições de opiniao
     {
-        free(opiniao[j]);
+        free(opiniao[j]); // libera a memoria de opniao na posição j
     }
-    free(opiniao);
+    free(opiniao); // libera a memoria alocada em opiniao
 
     return 0;
 }
