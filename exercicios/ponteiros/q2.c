d611235107d875308f8f10bdddf778b83b90438d
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-24 21:16:24 -0300

    Update q2.c

--- exercicios/ponteiros/q2.c
+++ exercicios/ponteiros/q2.c
@@ -1,8 +1,17 @@
+#include <stdio.h>
+
 int main(void)
 {
-    int x, *p;
-    x = 100;
-    p = x;
-    printf(“Valor de p = %p\tValor de *p = %d”, p, *p);
-    return 0;
+    int x, *p; // declarando as variáveis
+    x = 100;   // atribui 100 a x
+    p = x;     // p recebe x
+    printf("Valor de p = %p\tValor de *p = %d", p, *p);
 }
+
+/**
+ * a) advertencia
+ * b) pois o programa tenta imprimir com os tipos errados
+ * c) não
+ * d) p=&x;
+ * e) sim
+ */
